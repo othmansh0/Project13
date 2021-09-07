add6985681d4d4f7a3e56c9278ac241187e7e4f5
Othman Shahrouri <othmanshahrouri@gmail.com>
2021-09-07 09:31:32 +0300

    Design UI and implement importing picture

--- Project13/ViewController.swift
+++ Project13/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    var currentImage: UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        title = "Instafilter "
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+        
     }
 
-
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+    
+    @objc func importPicture(){
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    
+    
 }
 
